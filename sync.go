44f2a1a23fe569ed78652c1deec9de095bffbb8c
kael0927 <1192045470@qq.com>
2026-09-05 22:57:15 +0800

    调试

--- sync.go
+++ sync.go
@@ -7,16 +7,19 @@ import(
 var wg sync.WaitGroup
 
 func download(url string){
+	time.Sleep(time.Millisecond*50)
+	//wg.Add(1)
 	fmt.Println("start to download",url)
-	time.Sleep(time.Millisecond * 500)//模拟耗时操作
+	//模拟耗时操作
 	wg.Done()
 }
 
 func main(){
-	for i:=0; i < 3; i++{
+	for i:=0; i < 100; i++{
 		wg.Add(1)
 		go download("a.com/" + string(i+'0'))
 	}
+	time.Sleep(time.Millisecond)
 	wg.Wait()
 	fmt.Println("Done!")
 }
