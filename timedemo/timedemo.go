f6f325782343ca55a8df138ca1b6ec831741fb65
kael0927 <1192045470@qq.com>
2026-09-06 21:06:03 +0800

    time包

--- timedemo/timedemo.go
+++ timedemo/timedemo.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"time"
+	
 )
 func main(){
 	//当前时间
@@ -13,11 +14,21 @@ func main(){
 	t2 := time.Date(2006,9,27,11,43,5,0,time.Local)
 	fmt.Println(t2)
 
-	s3 := "1999年01月23日"
-	t3,err := time.Parse("2006年01月02日",s3)
-	if err !=nil{
-		fmt.Println(err)
-	}
-	fmt.Println(t3)
-	fmt.Printf("%T",t3)
+	//s3 := "1999年01月23日"
+	//t3,err := time.Parse("2006年01月02日",s3)
+	//if err !=nil{
+	//	fmt.Println(err)
+	//}
+	//fmt.Println(t3)
+	//fmt.Printf("%T",t3)
+	fmt.Println(t1.String())
+	year,mouth,day := t1.Date()
+	fmt.Println(year,mouth,day)
+	hour,min,sec := t1.Clock()
+	fmt.Println(hour,min,sec)
+	year2 := t1.Year()
+	fmt.Println(year2)
+	fmt.Println(t1.YearDay())
+	timeStamp := t1.Unix()
+	fmt.Println(timeStamp)
 }
